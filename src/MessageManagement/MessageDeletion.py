3dff58e5691c2433ddf3feedb40e60786a527f79
Ronnie Westhead <rwesthead2005@icloud.com>
2026-03-16 22:34:23 +0000

    {MessageDeletion.py} - fix: purge deletes most recent message rather than deleting the original message saying purge was done.

--- src/MessageManagement/MessageDeletion.py
+++ src/MessageManagement/MessageDeletion.py
@@ -11,8 +11,6 @@ async def purge(message: discord.Message):
             await message.channel.send(f'<@{message.author.id}>, you need to specify the number of messages to delete.')
             return
         await message.channel.purge(limit=amount + 1)
-        await message.channel.send(f'{amount} messages have been deleted by {message.author}.')
-        time.sleep(5)
-        await message.channel.purge(limit=1)
+        await message.channel.send(f'{amount} messages have been deleted by {message.author}.', delete_after=5)
     else:
         await message.channel.send(f'<@{message.author.id}>, you do not have permission to delete messages.')
