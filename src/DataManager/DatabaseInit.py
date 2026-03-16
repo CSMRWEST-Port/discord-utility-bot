acbb2265ed19dc60ca5ea066b094ae0d3dd28304
Ronnie Westhead <rwesthead2005@icloud.com>
2026-03-16 22:18:27 +0000

    {DatabaseInit.py} - fix: guild_id field declared as text now

--- src/DataManager/DatabaseInit.py
+++ src/DataManager/DatabaseInit.py
@@ -4,7 +4,7 @@ import psycopg2
 async def initialise_database(connection: psycopg2.extensions.connection):
     with connection.cursor() as cursor:
         cursor.execute('''CREATE TABLE IF NOT EXISTS guild_preferences (
-                                guild_id INTEGER NOT NULL PRIMARY KEY,
+                                guild_id TEXT NOT NULL PRIMARY KEY,
                                 auto_role TEXT NOT NULL,
                                 welcome_channel TEXT NOT NULL,
                                 welcome_message TEXT NOT NULL,
